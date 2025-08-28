923cb67202cd948babcf7eb2c6a8fdbdeed3aa14
majidali137 <majidaliwp69@gmail.com>
2025-08-28 11:44:15 +0500

    fiexed errors

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase config from environment
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  authDomain: "urbannest-8566e.firebaseapp.com",
+  projectId: "urbannest-8566e",
+  storageBucket: "urbannest-8566e.appspot.com",
+  messagingSenderId: "1035465573282",
+  appId: "1:1035465573282:web:104fa09b2d61ba8108a8fb",
+  measurementId: "G-TD4V6GS3ZN",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
