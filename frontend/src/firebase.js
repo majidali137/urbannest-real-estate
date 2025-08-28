47e4d6fb2b2996120476542df2b505f546d28a09
majidali137 <majidaliwp69@gmail.com>
2025-08-28 10:56:33 +0500

    update

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
 
+// Firebase config from environment
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "urbannest-8566e.firebaseapp.com",
-  projectId: "urbannest-8566e",
-  storageBucket: "urbannest-8566e.appspot.com",
-  messagingSenderId: "1035465573282",
-  appId: "1:1035465573282:web:104fa09b2d61ba8108a8fb",
-  measurementId: "G-TD4V6GS3ZN",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
